1b12417283a0dc4ec652e20a9a39b8e8dfe335f8
Sarah-F <60114072+Sarah-F@users.noreply.github.com>
2023-03-28 11:25:25 -0400

    Create webapp.py

---
+++ webapp.py
@@ -0,0 +1,15 @@
+@app.route("/profile", methods=["GET"])
+def profile():
+    """Fetching a protected resource using an OAuth 2 token.
+    """
+    github = OAuth2Session(client_id, token=session['oauth_token'])
+    return jsonify(github.get('https://api.github.com/user').json())
+
+
+if __name__ == "__main__":
+    # This allows us to use a plain HTTP callback
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = "1"
+
+    app.secret_key = os.urandom(24)
+    # app.run(debug=True)
+    app.run(ssl_context="adhoc")
